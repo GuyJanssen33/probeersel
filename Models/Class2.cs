855cf16405c5306483c26e8841190d2ec632212c
GuyJanssen33 <r0237357@student.thomasmore.be>
2022-11-13 20:12:22 +0100

    en nog wat code in een aftakking

---
+++ Models/Class2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probeersel.Models
+{
+    internal class Class2
+    {
+           {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int? PublisherId { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? Advance { get; set; }
+        public int? Royalty { get; set; }
+        public int? YtdSales { get; set; }
+        public string? Notes { get; set; }
+        public DateTime PubDate { get; set; }
+        public ICollection<Sale> Sales { get; set; }
+        public Publisher Publisher { get; set; }
+        public ICollection<TitleAuthor> TitlesAuthors { get; set; }
+    }
+}
