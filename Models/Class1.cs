d94629d30f84788a6eb20f38a78c4dc61a523a99
GuyJanssen33 <r0237357@student.thomasmore.be>
2022-11-13 20:07:53 +0100

    nog wat proberen

--- Models/Class1.cs
+++ Models/Class1.cs
@@ -2,6 +2,18 @@
 {
     public class Class1
     {
-
+        public class Sale
+        {
+            public int Id { get; set; }
+            public int StoreId { get; set; }
+            public string OrderNumber { get; set; }
+            public DateTime OrderDate { get; set; }
+            public int Quantity { get; set; }
+            public string? PayTerms { get; set; }
+            public int TitleId { get; set; }
+            public Title Title { get; set; }
+            public Store Store { get; set; }
+        }
     }
+}
 }
---
+++ Models/Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probeersel.Models
+{
+    internal class Class2
+    { 
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string? Minit { get; set; }
+        public string Lastname { get; set; }
+        public int JobId { get; set; }
+        public string? JobLevel { get; set; }
+        public int PublisherId { get; set; }
+        public DateTime HireDate { get; set; }
+        public Job Job { get; set; }
+        public Publisher Publisher { get; set; }
+    }
+}
+    }
+}
