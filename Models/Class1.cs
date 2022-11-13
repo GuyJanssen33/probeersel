47b34f9cebee55e0f1c38a1e4e61f8945ffbd74e
GuyJanssen33 <r0237357@student.thomasmore.be>
2022-11-13 20:14:36 +0100

    en nog een beetje code in dden een ander aftakking van de dev bracnh

--- Models/Class1.cs
+++ Models/Class1.cs
@@ -1,7 +1,50 @@
-﻿namespace Models
+﻿using DAL;
+using System.Collections.ObjectModel;
+
+namespace Models
 {
     public class Class1
     {
+        public class MainViewModel : BaseViewmodel
+        {
+            private string _naam = "";
+            private ObservableCollection<Store> _stores;
 
+            public override string this[string columnName]
+            {
+                get
+                {
+                    return "";
+                }
+            }
+
+            public ObservableCollection<Store> Stores
+            {
+                get { return _stores; }
+                set
+                {
+                    _stores = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            public string Naam
+            {
+                get { return _naam; }
+                set
+                {
+                    _naam = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            public void Zoeken()
+            {
+                //lijst storen ophalen
+                List<Store> lijstStores = DatabaseOperations.OphalenWinkelsViaWinkelnaam(Naam);
+                Stores = new ObservableCollection<Store>(lijstStores);
+            }
+        }
     }
+}
 }
